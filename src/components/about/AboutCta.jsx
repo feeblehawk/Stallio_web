e481731aab35e50f22bfe97220f69c966dc3020a
feeblehawk <ammartariq178@gmail.com>
2026-08-15 21:59:57 +0500

    move componets to page-specific sections

--- src/components/about/AboutCta.jsx
+++
@@ -1,6 +0,0 @@
-import FinalCTA from '../home/FinalCta'
-
-/** Re-export unified FinalCTA component for About page */
-const AboutCta = () => <FinalCTA />
-
-export default AboutCta
--- src/components/features/FeatureCta.jsx
+++
@@ -1,6 +0,0 @@
-import FinalCTA from '../home/FinalCta'
-
-/** Re-export unified FinalCTA component for Features page */
-const FeatureCta = () => <FinalCTA />
-
-export default FeatureCta
